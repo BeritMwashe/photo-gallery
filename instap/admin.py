4703d99cb9266bab0aa586d242129e18038602c1
Amukozoberit <mwasheb@gmail.com>
2021-09-14 09:00:11 +0300

    updated admin page

--- instap/admin.py
+++ instap/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 # Register your models here.
 from django.contrib.auth.models import User
-from .models import Employee, Post
+from .models import DisLiked, Employee, Followers, LikeClass, Post, commets
 
 
 
@@ -18,3 +18,7 @@ class UserAdmin(BaseUserAdmin):
 admin.site.unregister(User)
 admin.site.register(User,UserAdmin)
 admin.site.register(Post)
+admin.site.register(Followers)
+admin.site.register(LikeClass)
+admin.site.register(DisLiked)
+admin.site.register(commets)
